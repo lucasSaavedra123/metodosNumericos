8d26405abcdaf7727c0b3aa42d19c9eaba64fc23
lucassaavedra123 <lucasarielsaavedra@hotmail.com>
2020-03-16 19:18:21 -0300

    Abro el repositorio

---
+++ parte1Ejercicio3.m
@@ -0,0 +1,39 @@
+resultado = 0;
+disp("Ejercicio 3:\n");
+
+disp("i)");
+resultado = 1 +eps/2+eps/2;
+disp("Resultado:");
+disp(resultado);
+disp("El resultado termina siendo 0 ya que 'eps/2' no es un valor representable\n (estando normalizado) dentro del sistema.\n Por lo tanto, los toma como 0 y el resultado termina siendo 1\n");
+
+disp("ii)");
+resultado = (1 +eps/2+eps/2) - 1;
+disp("Resultado:");
+disp(resultado);
+disp("\n");
+
+disp("iii)");
+resultado = eps/2+eps/2+1;
+disp("Resultado:");
+disp(resultado);
+disp("DUDA\n");
+
+disp("iv)");
+resultado = (eps/2+eps/2+1)-1;
+disp("Resultado:");
+disp(resultado);
+disp("DUDA\n");
+
+disp("v)");
+resultado = 0.2+0.1-0.3;
+disp("Resultado:");
+disp(resultado);
+disp("\n");
+
+
+disp("vi)");
+resultado = 0.2-0.3+0.1;
+disp("Resultado:");
+disp(resultado);
+disp("\n");
